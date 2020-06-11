beb72d36ed7a3c4583fdb32d2569113ef13eb658
kazo23 <ka-zo@live.com>
2020-06-11 12:15:29 +0100

    Setup Db

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -7,26 +7,9 @@ const config = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
-};
-
-// const config = {
-//     apiKey: "AIzaSyBnHzDYc6uvj_BtRxM_-HxeHKzEePyTafM",
-//     authDomain: "expensify-a55e8.firebaseapp.com",
-//     databaseURL: "https://expensify-a55e8.firebaseio.com",
-//     projectId: "expensify-a55e8",
-//     storageBucket: "expensify-a55e8.appspot.com",
-//     messagingSenderId: "571122891577",
-//     appId: "1:571122891577:web:be8520b32fe2e67b5646e8",
-//     measurementId: "G-WZ0X4XMJHY"
-// };
-
+}
 
 firebase.initializeApp(config);
-
-// firebase.database().ref().set({
-//   name: 'Tatenda Chikaka'
-// })
-
 const database = firebase.database();
 const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
