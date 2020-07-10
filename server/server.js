d73fa720b4e9dbbd62c27c4c5d0ca806f327cbaf
kazo23 <ka-zo@live.com>
2020-07-10 11:33:11 +0200

    Update 09/07/20 v2

--- server/server.js
+++ server/server.js
@@ -5,11 +5,11 @@ const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
-
+//route 1
 app.get('/app', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
-
+//route 2
 app.get('/info', (req, res) => {
   res.sendFile(path.join(publicPath, 'info.html'))
 })
